256aa47f4a022000579a4f51106f115ee994e6e1
antonio <amorenovalls@gmail.com>
2016-11-10 14:49:27 +0100

    Cambios en la interfaz (aún no es definitiva), cambios de código.

--- app/src/main/java/com/example/antonio/calculator/MainActivity.java
+++ app/src/main/java/com/example/antonio/calculator/MainActivity.java
@@ -8,41 +8,82 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    TextView tvResult;
+    TextView tvOperacion;
     Button b1;
-    int result = 0; // Guardamos el resultado de las operaciones realizadas.
+    Double num1, num2, resultado;
+    String operador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvResult = (TextView) findViewById(R.id.textView);
-        tvResult.setText("");
+        tvOperacion = (TextView) findViewById(R.id.textView);
+        tvOperacion.setText("");
     }
 
     public void onClick(View view) {
-        tvResult.setText(tvResult.getText() + view.getTag().toString());
+        tvOperacion.setText(tvOperacion.getText() + view.getTag().toString());
     }
 
     public void onClear(View view) {
-        tvResult.setText("");
-        result = 0;
+        tvOperacion.setText("");
+        resultado = 0.0;
+        num1 = 0.0;
+        num2 = 0.0;
     }
 
-    public void onSum() {
+    public void onResult(View view) {
         try {
-            if (tvResult.getText() != "") {
-                result = result + Integer.parseInt((String) tvResult.getText());
-                System.out.println(result);
-                tvResult.setText("");
-            } else {
-                Toast.makeText(this, "En blanco", Toast.LENGTH_LONG).show();
+            num2 = Double.parseDouble(tvOperacion.getText().toString());
+            switch (operador) {
+                case "+":
+                    resultado = num1 + num2;
+                    break;
+                case "-":
+                    resultado = num1 - num2;
+                    break;
+                case "*":
+                    resultado = num1 * num2;
+                    break;
+                case "/":
+                    resultado = num1 / num2;
+                    break;
             }
-
+            tvOperacion.setText(resultado.toString());
+        } catch (NullPointerException npe) {
+            Toast.makeText(this, "No results", Toast.LENGTH_SHORT).show();
+            tvOperacion.setText("0");
         } catch (Exception e) {
             e.getMessage();
         }
 
+
+    }
+
+
+    public void onClickSuma(View view) {
+        operador = "+";
+        onClickOperacionCapturaNumero1(view);
+    }
+
+    public void onClickResta(View view) {
+        operador = "-";
+        onClickOperacionCapturaNumero1(view);
+    }
+
+    public void onClickMultiplicacion(View view) {
+        operador = "*";
+        onClickOperacionCapturaNumero1(view);
+    }
+
+    public void onClickDivision(View view) {
+        operador = "/";
+        onClickOperacionCapturaNumero1(view);
+    }
+
+    public void onClickOperacionCapturaNumero1(View miView) {
+        num1 = Double.parseDouble(tvOperacion.getText().toString());
+        tvOperacion.setText("");
     }
 
 }
